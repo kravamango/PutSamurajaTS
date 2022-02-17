e97badcd05dd41637849ca0b738e9fe56408e24d
kravamango <kravamango@gmail.com>
2022-02-17 18:08:29 +0300

    29 test error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import './Header.css';
 const Header = () => {
     return <header className='header'>
         <img src='https:cdn.logo.com/hotlink-ok/logo-social.png'/>
+        <img src='https:cdn.logo.com/hotlink-ok/logo-social.png'/>
     </header>
 }
 export default Header;
